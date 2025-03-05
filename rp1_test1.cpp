d492d58288373b6eb512e00d649580a1496cc1bb
Ando-Hideyuki <hideyuki.a@gmail.com>
2025-03-05 12:46:10 +0000

    変更内容を記述

--- rp1_test1.cpp
+++ rp1_test1.cpp
@@ -8,11 +8,22 @@ RP1_GPIO rp1;
 int main(void)
 {
   rp1.begin();
+
+  //おまじない
+  rp1.pinMode(22, rp1.OUTPUT);
+  rp1.digitalWrite(22, rp1.LOW);//LOW HIGH TOGGLE
+  rp1.pinMode(22, rp1.INPUT);
+  
   rp1.pinMode(PIN, rp1.OUTPUT);
+  rp1.pinMode(4, rp1.OUTPUT);
+  rp1.pinMode(23, rp1.INPUT);
   while (1)
   {
+    rp1.digitalWrite(4, rp1.LOW);//LOW HIGH TOGGLE
     rp1.digitalWrite(PIN, rp1.TOGGLE);//LOW HIGH TOGGLE
-    //usleep(500000); // wait 0.5 Sec
+    usleep(500000); // wait 0.5 Sec
+    rp1.digitalWrite(4, rp1.HIGH);//LOW HIGH TOGGLE
+    usleep(500000); // wait 0.5 Sec
     //usleep(1);
   }
   rp1.end();
